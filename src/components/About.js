72291c936d9766914473bc35cbbc703d916c66e8
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-25 15:49:57 +0300

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src="demo.png" alt= "I made this" />
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt= "I made this" />
     </div>
   );
 }
